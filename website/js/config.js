b36a3d5dc45ecf30d5459563083bfcb09662eb11
Christian Schmitt <christian.schmitt@iunera.com>
2025-06-04 12:20:40 +0200

    add license and copyright

--- website/js/config.js
+++ website/js/config.js
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2025.  Tim Frey, Christian Schmitt
+ *
+ * Licensed under the OPEN COMPENSATION TOKEN LICENSE (the "License").
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at <a href="https://www.license-token.com/license/text">https://www.license-token.com/license/text</a>
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either expressed or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @octl.sid: 5fecd757-5fec-d757-d757-00005fb33b80
+ *
+ */
+
 // Configuration Management
 export class Config {
     static DEFAULT_TYPE = 'Article';
@@ -5,9 +22,9 @@ export class Config {
     static DEFAULT_BASE_URL = 'https://www.iunera.com/blog/';
     static DEFAULT_DATE = '2025-06-01';
     static DEFAULT_AUTHOR = ['Christian Schmitt', 'Dr. Tim Frey'];
-    static DEFAULT_PUBLISHER = { 
-        '@type': 'Organization', 
-        name: 'Iunera', 
+    static DEFAULT_PUBLISHER = {
+        '@type': 'Organization',
+        name: 'Iunera',
         '@id': 'https://www.iunera.com',
         url: 'https://www.iunera.com/',
         address: {
--- website/js/ui-controller.js
+++ website/js/ui-controller.js
@@ -1,5 +1,22 @@
+/*
+ * Copyright (C) 2025.  Tim Frey, Christian Schmitt
+ *
+ * Licensed under the OPEN COMPENSATION TOKEN LICENSE (the "License").
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at <a href="https://www.license-token.com/license/text">https://www.license-token.com/license/text</a>
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either expressed or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @octl.sid: 5fecd757-5fec-d757-d757-00005fb33b80
+ *
+ */
+
 // UI Controller
-import { Config } from './config.js';
+import {Config} from './config.js';
 // Import the library directly as a script in index.html and access it as a global variable
 // The library will be available as window.MarkdownToJsonLd
 
